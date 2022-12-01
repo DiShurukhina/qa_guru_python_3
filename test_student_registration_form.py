65a49b5b141a8dacaf49ca6233847ee7e03339ca
DiShurukhina <dolbunok@gmail.com>
2022-12-01 11:02:27 +0300

    first commit

---
+++ test_student_registration_form.py
@@ -0,0 +1,31 @@
+from selene.support.shared import browser
+from selene import be, command
+
+
+browser.config.hold_browser_open = True
+browser.open('https://demoqa.com/automation-practice-form')
+browser.element('[id="firstName"]').should(be.blank).type('Dinara')
+browser.element('[id="lastName"]').should(be.blank).type('Shurukhina')
+browser.element('[id="userEmail"]').should(be.blank).type('testEmail@gmail.com')
+browser.element('[for="gender-radio-2"]').click()
+browser.element('[id="userNumber"]').should(be.blank).type('81234567890')
+browser.element('[id="dateOfBirthInput"]').click()
+browser.element('[class="react-datepicker__month-select"').send_keys('May')
+browser.element('[class="react-datepicker__year-select"').send_keys('1986')
+browser.element('[class="react-datepicker__day react-datepicker__day--014"').click()
+browser.element('[id="subjectsInput"]').should(be.blank).type('math, history, Informatics')
+browser.element('[for="hobbies-checkbox-2"]').click()
+browser.element('[for="hobbies-checkbox-3"]').click()
+browser.element('[id="uploadPicture"]').send_keys('C:/AQA/qa_guru_python_1/photo_2016-08-25_20-45-23.jpg')
+browser.element('[id="currentAddress"]').type('Moscow, Tverskaya str, 19, 173')
+browser.element('[id="state"]').perform(command.js.scroll_into_view)
+browser.element('[id="react-select-3-input"]').send_keys('NCR').press_enter()
+browser.element('[id="react-select-4-input"]').send_keys('Delhi').press_enter()
+browser.element('[id="submit"]').press_enter()
+
+
+
+
+
+
+
